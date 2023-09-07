d036c50b42769d4a2f9ed15edc2f51b9fe10f9ba
keshav-chandra <keshavchandra2222@gmail.com>
2023-09-07 17:05:24 +0530

    first commit

---
+++ spring-boot-001/src/test/java/com/org/springboot001/SpringBoot001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.springboot001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
